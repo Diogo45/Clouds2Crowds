35a0ed254dae8660267f753dbfa569fccee225c1
André Antonitsch <andre.antonitsch@acad.pucrs.br>
2019-03-07 14:32:41 -0300

    Final commit for CGI

--- NewAndImprovedBioCrowds/Assets/BioCrowds/Scripts/Components/Agent.cs
+++ NewAndImprovedBioCrowds/Assets/BioCrowds/Scripts/Components/Agent.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
 using Unity.Entities;
 using Unity.Collections;
 using Unity.Mathematics;
-using System.Linq;
 using Unity.Transforms;
 using Unity.Jobs;
 using UnityEngine.Experimental.PlayerLoop;
