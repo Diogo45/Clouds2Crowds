b2bec7cc5eb1539d79b72e3aa92525cb79b85dd1
amyrneto <amyrneto@gmail.com>
2019-10-11 11:06:17 -0300

    Initial version of NewAndImprovedBioCrowds

---
+++ NewAndImprovedBioCrowds/Assets/BioCrowds/Scripts/AlignWithCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignWithCamera : MonoBehaviour
+{
+	Transform camTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+		camTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 fwd = camTransform.position - transform.position;
+		Vector3 upwd = camTransform.up;
+		Quaternion rotation = Quaternion.LookRotation(fwd, upwd);
+		Vector3 euler = rotation.eulerAngles;
+		euler.x += 90;
+		rotation = Quaternion.Euler(euler);
+		transform.rotation = rotation;
+    }
+}
